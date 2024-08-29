485195470a6f5c5798237f1be8adf60384d5e79a
Akshay <aksh.ed@outlook.com>
2024-08-29 11:28:10 +0530

    revert

--- client/src/app/models/client-verification.ts
+++
@@ -1,9 +0,0 @@
-export interface ClientVerification {
-    clientId: string;
-    token: string;
-    dateOfBirth: string;
-    country: string;
-    postalCode: string;
-    identification: { type: string; value: string }[];
-  }
-  
