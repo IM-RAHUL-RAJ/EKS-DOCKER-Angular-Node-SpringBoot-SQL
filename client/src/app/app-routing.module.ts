f3f5cdbac63af18903cba990c34fca75fec2c541
a752169 <aksh.ed@outlook.com>
2024-09-04 10:19:24 +0530

    integration of portfolio watchlist report

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './components/landing/homepage/homepage.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
-import { InvestmentPreferences } from './models/investment-preferences';
 import { InvestmentPreferencesComponent } from './components/investment-preferences/investment-preferences.component';
 import { TradingHistoryComponent } from './components/trading-history/trading-history.component';
 import { TradeComponent } from './components/trade/trade.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
 const routes: Routes = [
     {
@@ -37,6 +37,10 @@ const routes: Routes = [
     {
         path: 'history',
         component: TradingHistoryComponent
+    },    
+    {
+        path: 'portfolio',
+        component: PortfolioComponent
     },
     {
         path: "**",
