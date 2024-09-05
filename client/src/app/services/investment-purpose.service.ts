cd651e7587fa2028efd88ce10038669a10411451
Tanvi129 <tanvigupta129@gmail.com>
2024-09-05 09:55:46 +0530

    UI and functionality

--- client/src/app/services/investment-purpose.service.ts
+++ client/src/app/services/investment-purpose.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
 import { InvestmentPreferences } from '../models/investment-preferences';
 import { Observable, of } from 'rxjs';
+import { ClientService } from './client.service';
+import { Client } from '../models/client';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InvestmentPurposeService {
 
-  private investmentPreference = new InvestmentPreferences("DPQR100","College Fund","AVERAGE","60,001 - 80,000","0-5 years",false)
-
-  constructor() { }
+  private investmentPreference! : InvestmentPreferences
+  private cliendID!: string
+  constructor(private clientService: ClientService) {
+   this.cliendID = '10001'
+   }
 
   getInvestmentPreference() : Observable<InvestmentPreferences> {
+    this.investmentPreference = new InvestmentPreferences(this.cliendID,"College Fund","AVERAGE","60,001 - 80,000","0-5 years",false)
     return of(this.investmentPreference)
   }
 
