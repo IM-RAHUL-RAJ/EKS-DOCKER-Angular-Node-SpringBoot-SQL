506619b384d4317eb83a2e799722899ca7bc248a
Rahul Prasanna <prasannarahul22@gmail.com>
2024-09-22 19:03:54 +0530

    "add investment preference dao"

---
+++ server/business/stock-stream/src/main/java/com/capstone/integration/InvestmentPreferenceDaoImpl.java
@@ -0,0 +1,35 @@
+package com.capstone.integration;
+
+import com.capstone.models.InvestmentPreference;
+
+public class InvestmentPreferenceDaoImpl implements InvestmentPreferenceDoa {
+
+	public InvestmentPreferenceDaoImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public InvestmentPreference getInvestmentPreference(String clientId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public InvestmentPreference addInvestmentPreference(InvestmentPreference investmentPreference) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public InvestmentPreference updateInvestmentPreference(InvestmentPreference investmentPreference) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public InvestmentPreference deleteInvestmentPreference(InvestmentPreference investmentPreference) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
